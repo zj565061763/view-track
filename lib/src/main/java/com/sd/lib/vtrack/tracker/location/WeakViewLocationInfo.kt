ca3d9c66c1aa3b8544268b638500c42e602ebc91
zhengjun <565061763@qq.com>
2022-06-06 09:42:59 +0800

    kotlin WeakViewLocationInfo

--- lib/src/main/java/com/sd/lib/vtrack/tracker/location/WeakViewLocationInfo.kt
+++ lib/src/main/java/com/sd/lib/vtrack/tracker/location/WeakViewLocationInfo.kt
@@ -1,58 +1,38 @@
-package com.sd.lib.vtrack.tracker.location;
+package com.sd.lib.vtrack.tracker.location
 
-import android.view.View;
+import android.view.View
+import com.sd.lib.vtrack.tracker.ViewTracker.ViewLocationInfo
+import java.lang.ref.WeakReference
 
-import androidx.annotation.NonNull;
+open class WeakViewLocationInfo : ViewLocationInfo {
+    private var _viewRef: WeakReference<View>? = null
 
-import com.sd.lib.vtrack.tracker.ViewTracker;
-
-import java.lang.ref.WeakReference;
-
-public class WeakViewLocationInfo implements ViewTracker.ViewLocationInfo {
-    private WeakReference<View> mView;
-    private final int[] mLocation = {0, 0};
-
-    @Override
-    public boolean isReady() {
-        final View view = getView();
-        if (view == null) return false;
-        if (!view.isAttachedToWindow()) return false;
-        return view.getWidth() > 0 && view.getHeight() > 0;
-    }
-
-    @Override
-    public View getView() {
-        return mView == null ? null : mView.get();
-    }
-
-    @Override
-    public void setView(View view) {
-        final View old = getView();
-        if (old != view) {
-            mView = view == null ? null : new WeakReference<>(view);
-            onViewChanged(old, view);
+    override val isReady: Boolean
+        get() {
+            val view = _viewRef?.get() ?: return false
+            if (!view.isAttachedToWindow) return false
+            return view.width > 0 && view.height > 0
         }
+
+    override var view: View?
+        get() = _viewRef?.get()
+        set(value) {
+            val old = _viewRef?.get()
+            if (old !== value) {
+                _viewRef = if (value == null) null else WeakReference(value)
+                onViewChanged(old, value)
+            }
+        }
+
+    override val width: Int
+        get() = _viewRef?.get()?.width ?: 0
+
+    override val height: Int
+        get() = _viewRef?.get()?.height ?: 0
+
+    override fun getCoordinate(position: IntArray) {
+        _viewRef?.get()?.getLocationOnScreen(position)
     }
 
-    @Override
-    public int getWidth() {
-        final View view = getView();
-        return view == null ? 0 : view.getWidth();
-    }
-
-    @Override
-    public int getHeight() {
-        final View view = getView();
-        return view == null ? 0 : view.getHeight();
-    }
-
-    @Override
-    public void getCoordinate(@NonNull int[] position) {
-        final View view = getView();
-        if (view == null) return;
-        view.getLocationOnScreen(position);
-    }
-
-    protected void onViewChanged(View old, View view) {
-    }
+    protected open fun onViewChanged(old: View?, view: View?) {}
 }
