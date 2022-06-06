e3c0f872a8a6e1bab7e4567de521927fd51b921f
zhengjun <565061763@qq.com>
2022-06-06 11:46:32 +0800

    OnLayoutChangeUpdater

--- lib/src/main/java/com/sd/lib/vtrack/updater/impl/OnLayoutChangeUpdater.kt
+++ lib/src/main/java/com/sd/lib/vtrack/updater/impl/OnLayoutChangeUpdater.kt
@@ -1,30 +1,22 @@
-package com.sd.lib.vtrack.updater.impl;
+package com.sd.lib.vtrack.updater.impl
 
-import android.view.View;
-
-import androidx.annotation.NonNull;
-
-import com.sd.lib.vtrack.updater.BaseViewUpdater;
+import android.view.View
+import android.view.View.OnLayoutChangeListener
+import com.sd.lib.vtrack.updater.BaseViewUpdater
 
 /**
- * 通过{@link View.OnLayoutChangeListener}来实现更新
+ * 通过[View.OnLayoutChangeListener]来实现更新
  */
-public class OnLayoutChangeUpdater extends BaseViewUpdater {
-    @Override
-    protected final boolean startImpl(@NonNull View view) {
-        view.addOnLayoutChangeListener(mListener);
-        return true;
+class OnLayoutChangeUpdater : BaseViewUpdater() {
+
+    override fun startImpl(view: View): Boolean {
+        view.addOnLayoutChangeListener(_listener)
+        return true
     }
 
-    @Override
-    protected final void stopImpl(@NonNull View view) {
-        view.removeOnLayoutChangeListener(mListener);
+    override fun stopImpl(view: View) {
+        view.removeOnLayoutChangeListener(_listener)
     }
 
-    private final View.OnLayoutChangeListener mListener = new View.OnLayoutChangeListener() {
-        @Override
-        public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-            notifyUpdatable();
-        }
-    };
+    private val _listener = OnLayoutChangeListener { _, _, _, _, _, _, _, _, _ -> notifyUpdatable() }
 }
